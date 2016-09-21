3f6322b04d0ccdc014c58eb34c4cf7ae7da46a9d
Mate Retsagi <retsagimate@gmail.com>
2016-09-21 23:49:19 +0200

    design fixes, comments added to script

--- js/script.js
+++ js/script.js
@@ -1,24 +1,39 @@
 $(function() {
+    var animation = false;
 
+    // define function to select and display content
     function select(contentId) {
-        console.log('fade start');
-        $('#' + contentId).fadeIn(800, function() {
-            console.log('fade done');
-            $('#' + contentId).addClass('selected');
-        });
-        return false;
+        // prevent animation being triggered until previous animation is completed
+        if (!animation) {
+            animation = true;
+            // add .selected class when fadeIn completed
+            $('#' + contentId).fadeIn(800, function() {                
+                animation = false;
+                $('#' + contentId).addClass('selected');
+            });
+            return false;
+        }
     }
 
+    // trigger animation on click event (only if previous one has been completed)
     $('.nav li').click(function() {
-        var contentId = $(this).attr('id').slice(4);
-        console.log('puff start');
-        selected = $('.selected');
-        $('.dynContent').removeClass('selected');
-        selected.effect('puff', 600 , function() {
-            console.log('puff done');
-            select(contentId);
-        });
+        if (!animation) {
+            var contentId = $(this).attr('id').slice(4);
+            animation = true;
+            selected = $('.selected');
+            $('.dynContent').removeClass('selected');
+            selected.effect('puff', 600 , function() {
+                animation = false;
+                select(contentId);
+            });   
+        }
     });
+    
+    // prevent focus jumping back to the top of the page
+    $('.moreInfo').click(function(){
+        return false;
+    })
+    
 });
 
 
