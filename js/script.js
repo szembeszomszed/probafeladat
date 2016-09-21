747363d1054547c44c36e786f285a85f46dfffda
Mate Retsagi <retsagimate@gmail.com>
2016-09-21 22:40:13 +0200

    effect works

--- js/script.js
+++ js/script.js
@@ -1,46 +1,27 @@
-var main = function() {
-    console.log($( "#crm" ).hasClass('selected'));
+$(function() {
+
+    function select(contentId) {
+        console.log('fade start');
+        $('#' + contentId).fadeIn(800, function() {
+            console.log('fade done');
+            $('#' + contentId).addClass('selected');
+        });
+        return false;
+    }
+
     $('.nav li').click(function() {
-        $('.selected').effect('puff', 600);
-        $('.dynContent').removeClass('selected'); // remove selection from element
-        var cat = $(this).attr('id'); // grab id of clicked element
-        function select(selectedElement) {
-            var stringPos = 4; // position in string where slice starts ("nav-...")
-            var contentId = selectedElement.slice(stringPos); // remove "nav-" from string
-            setTimeout(function() {
-                $('#' + contentId).fadeIn(800); // add '.selected' class to element
-                $('#' + contentId).addClass('selected');
-            }, 800);
-            return false;   
-        }
-        select(cat); // function call
-
-/* alternative solution using switch
-        switch(cat) {
-            case "nav-crm" :
-                console.log("nav-crm clicked");
-                $('.dynContent').removeClass('selected');
-                $('#crm').addClass('selected');
-                break;
-            case "nav-panorama" :
-                $('.dynContent').removeClass('selected');
-                $('#panorama').addClass('selected');
-                break;
-            case "nav-finrep" :
-            console.log("finrep");
-                $('.dynContent').removeClass('selected');
-                $('#finrep').addClass('selected');
-                break;
-            case "nav-lbma" :
-                $('.dynContent').removeClass('selected');
-                $('#lbma').addClass('selected');
-                break;
-        } */       
-    })
-}
+        var contentId = $(this).attr('id').slice(4);
+        console.log('puff start');
+        selected = $('.selected');
+        $('.dynContent').removeClass('selected');
+        selected.effect('puff', 600 , function() {
+            console.log('puff done');
+            select(contentId);
+        });
+    });
+});
 
 
 
 
 
-$(document).ready(main);
